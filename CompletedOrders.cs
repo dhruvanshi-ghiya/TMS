9e2e410af071bfe08ca439385a20e7126c95177d
Dhruvanshi Ghiya <dhruvanshighiya7481@gmail.com>
2021-12-21 00:21:06 -0500

    Orders

---
+++ CompletedOrders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Logic
+{
+    class CompletedOrders
+    {
+
+        public static List<Order> Orders = new List<Order>();
+
+        public static List<Order> GetAll()
+        {
+            return Orders;
+        }
+
+        public static void Add(Order o)
+        {
+            Orders.Add(o);
+        }
+    }
+}
---
+++ PendingOrders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Logic
+{
+    class PendingOrders
+    {
+        public static List<Order> Orders = new List<Order>();
+
+        public static List<Order> GetAll()
+        {
+            return Orders;
+        }
+
+        public static void Add(Order o)
+        {
+            Orders.Add(o);
+        }
+    }
+}
